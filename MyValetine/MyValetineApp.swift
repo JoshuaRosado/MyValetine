2d41930192c928c1268291d01415891a2c770ad7
JoshuaRosado <joshua06rosado@gmail.com>
2025-01-29 14:26:22 -0600

    Initial Commit

---
+++ MyValetine/MyValetineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyValetineApp.swift
+//  MyValetine
+//
+//  Created by Joshua Rosado Olivencia on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MyValetineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyValetineTests/MyValetineTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyValetineTests.swift
+//  MyValetineTests
+//
+//  Created by Joshua Rosado Olivencia on 1/29/25.
+//
+
+import Testing
+@testable import MyValetine
+
+struct MyValetineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
