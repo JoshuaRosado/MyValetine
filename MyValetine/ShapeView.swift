5d2d94800d4e7d14ed00ece309a92f42c6bb2b6a
JoshuaRosado <joshua06rosado@gmail.com>
2025-02-01 10:29:27 -0600

    Shake animation added to envelope object

--- MyValetine/ShapeView.swift
+++ MyValetine/ShapeView.swift
@@ -22,7 +22,7 @@ struct Triangle: Shape {
 struct TheTriangle: View {
     var body: some View {
         Triangle()
-            .fill(Color.pink).brightness(0.3)
+            .fill(Color("pinkInside"))
             .frame(width: 300, height: 125)
             .position(x: 150, y:65)
             .cornerRadius(20)
@@ -33,8 +33,8 @@ struct TheTriangle: View {
 struct OpenedTriangle: View {
     var body: some View {
         Triangle()
-            .fill(Color.pink).brightness(0.6)
-            .frame(width: 300, height: 130)
+            .fill(Color("pinkTriangle"))
+            .frame(width: 310, height: 130)
             .position(x: 150, y:65)
             .cornerRadius(20)
             
@@ -45,7 +45,7 @@ struct OpenedTriangle: View {
 struct TheRectangle: View {
     var body: some View {
         Rectangle()
-            .fill(Color.pink).brightness(-0.3)
+            .fill(Color("pinkOutside"))
         
             .frame(width: 300, height: 200)
             .cornerRadius(20)
@@ -57,20 +57,22 @@ struct TheRectangle: View {
 
 struct ShapeView: View {
     @State private var isMessageOpen = false
+    @State private var shakeOffsetX: CGFloat = 0
+    @State private var shakeOffsetY: CGFloat = 0
     @State private var rotation : Double = 0
+    @State private var shakeAnimationTrigger = true
+    
+    let shakeDuration: Double = 0.3
+    let shakeInterval: Double = 2.0
+    
+    
+    
     
     var body: some View {
         ZStack{
-            ZStack{
-                
-                RoundedRectangle(cornerRadius: 60)
-                    .stroke(Color.brown, lineWidth: 20).brightness(-0.4)
-                    .ignoresSafeArea()
                 RoundedRectangle(cornerRadius: 20)
                     .fill(Color.red.opacity(0.1))
-                
                     .ignoresSafeArea()
-            }
             
             ZStack{
                 ZStack{
@@ -82,9 +84,9 @@ struct ShapeView: View {
                         
                         OpenedTriangle()
                             .rotation3DEffect(.degrees(180), axis: (x: 0, y: 0, z: rotation))
-                            .position(x:150, y: -90)
+                            .position(x:150, y: -88)
                             .animation(.easeInOut(duration: 20), value: rotation)
-                            .shadow(color:.black.opacity(0.2), radius: 10, x: 20, y: 15)
+                            .shadow(color:.black.opacity(0.2), radius: 20, x: 25, y: 2)
                             
                             
                     } else {
@@ -97,8 +99,9 @@ struct ShapeView: View {
                 .frame(width: 300, height: 200)
                 
                 Button("Open"){
-                    isMessageOpen.toggle()
+                    
                     withAnimation{
+                        isMessageOpen.toggle()
                         rotation = 1
                     }
                     
@@ -108,10 +111,37 @@ struct ShapeView: View {
                 .padding(.top, 125)
                 .buttonStyle(.plain)
             }
+            .offset(x: shakeOffsetX, y:shakeOffsetY)
+            .animation(
+                Animation.linear(duration: shakeDuration),
+                value: shakeAnimationTrigger
+            )
+            .onAppear(){
+                startShaking()
+            }
         }
+        
 //        .sheet(isPresented: $isMessageOpen){
 //            OpenedMessage()
 //        }
+    }
+    
+    private func startShaking() {
+        Timer.scheduledTimer(withTimeInterval: shakeInterval, repeats: true) { _ in
+            shakeAnimationTrigger.toggle()
+            withAnimation {
+                shakeOffsetX = 1.5
+                shakeOffsetY = 15
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + shakeDuration) {
+                withAnimation{
+                    shakeOffsetX = 0
+                    shakeOffsetY = 0
+                }
+            }
+            
+        }
     }
 }
 
